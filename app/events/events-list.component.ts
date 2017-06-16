18f2b14c1835c68272c7261146f1179f5cb27213
kelvin knigton <keltheceo@gmail.com>
2017-06-16 10:05:46 -0700

    just finished: adding a Site Header

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -5,29 +5,20 @@ import { Component } from '@angular/core';
   template: `
     <div>
       <h1>Upcoming Angular 4 Events</h1>
-      <hr>
-      <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-          <span>Location: {{event.location.address}}</span>
-          <span>&nbsp;</span>
-          <span>{{event.location.city}}, {{event.location.country}}</span>
-        </div>
-      </div>
+      <hr/>
+      <event-thumbnail #burrito [event]="event1"></event-thumbnail>
     </div>
   `
 })
 
+// Parent - EventThumbnailComponent 
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Connect', 
     date: '9/25/2055',
     time: '10:00 am',
-    price: 599.99,
+    price: 599.99, 
     imageUrl: '/app/assets/images/angularconnect-shield.png',
     location: {
       address: '1057 DT',
@@ -35,4 +26,5 @@ export class EventsListComponent {
       country: 'England'
     }
   }
+
 }
