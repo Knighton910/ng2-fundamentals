cafbea84b2c4c3a0c3a7ca8a5c20ede863afc50d
kelvin knigton <keltheceo@gmail.com>
2017-06-16 17:42:42 -0700

    finished: Guarding Against Route De-activation

--- app/app.module.ts
+++ app/app.module.ts
@@ -28,9 +28,23 @@ import { EventRouteActivator }from './events/event-details/event-route-activator
     CreateEventComponent,
     Error404Component
     ],
-  providers: [EventService, ToastrService, EventRouteActivator],
+  providers: [
+    EventService, 
+    ToastrService, 
+    EventRouteActivator,
+    { 
+      provide: 'canDeactivateCreateEvent', 
+      useValue: checkDirtyState 
+    }
+    ],
   bootstrap: [EventsAppComponent]
 })
-export class AppModule {
+export class AppModule {}
 
+function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you want to cancel?')
+ return true
 }
+
+ 
