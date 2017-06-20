773757fdd35ba216de9ffebccf754209e3f3e950
kelvin knigton <keltheceo@gmail.com>
2017-06-19 17:02:17 -0700

    finished: Passing Data out of a Child Comp..

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service'
 import { ActivatedRoute } from '@angular/router'
-import { IEvent } from '../shared/index'
+import { IEvent, ISession } from '../shared/index'
 
 @Component({
   selector: '',
@@ -27,4 +27,16 @@ export class EventDetailsComponent implements OnInit {
     this.addMode = true 
   }
 
-}   
+  saveNewSession(session:ISession) {
+    const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
+    session.id = nextId + 1
+    this.event.sessions.push(session)
+    this.eventService.updateEvent(this.event)
+    this.addMode = false
+  }
+
+  cancelAddSession() {
+    this.addMode = false 
+  }
+
+}    
